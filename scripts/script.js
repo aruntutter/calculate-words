ed1ba6ff436537e722a76a1db1baca47da2354aa
Arun Kumar R <aruntutter19@gmail.com>
2023-07-15 12:43:58 +0530

    javascript logic added

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
-"use strict";
+'use strict'
+
+document.addEventListener("DOMContentLoaded", function () {
+  // select elements
+  let button = document.querySelector("#btn");
+  let input = document.getElementById("input");
+  let output = document.getElementById("output");
+
+  // add event listener
+  button.addEventListener("click", function () {
+    let text = input.value.trim();
+    let wordCount = countWords(text);
+    output.innerHTML = "<h2>" + wordCount + "</h2>";
+  });
+});
+
+function countWords(text) {
+  let words = text.split(/\s+/);
+  return words.length + ' Words';
+}
